578ad67e67793eec913ecd944397619b82562957
student-t <WongMingYin@gmail.com>
2015-03-13 23:03:38 +1100

    Added a project file for MSVC.

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,5 @@ int main(int argc, char * const argv[])
     AlignAnalyzer::analyze("accepted_hits.sam");
    
     
-    struct AlignAnalyzer
-    {
-        /*
-         * Analyze sequence alignments for sequins for a given SAM file.
-         */
-        
-        static void analyze(const std::string &file);
 
-    
-    
-    return 0;
 }
